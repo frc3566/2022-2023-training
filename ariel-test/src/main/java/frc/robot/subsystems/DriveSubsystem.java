ad2f092cc4f0f7e785779759737e12c3c665f87e
Ariel <ariel.kerry@gmail.com>
2022-12-02 17:47:32 -0500

    changed to Talon

--- ariel-test/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ ariel-test/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,52 +5,44 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  private CANSparkMax left1;
-  private CANSparkMax left2;
-  private CANSparkMax left3;
-  private CANSparkMax right1;
-  private CANSparkMax right2;
-  private CANSparkMax right3;
+  private WPI_TalonSRX left1;
+  private WPI_TalonSRX left2;
+  private WPI_TalonSRX right1;
+  private WPI_TalonSRX right2;
 
   // Set inverted to false
 
   /** Creates a new ExampleSubsystem. */
   public DriveSubsystem() {
-    left1 = new CANSparkMax(4, MotorType.kBrushless);
-    left1.setInverted(false);;
+    left1 = new WPI_TalonSRX(1);
+    left1.setInverted(false);
     // leftController = left1.getPIDController();
     // setControler(leftController);
 
-    left2 = new CANSparkMax(5, MotorType.kBrushless);
+    left2 = new WPI_TalonSRX(2);
     left2.follow(left1); // Makes this motor mirror left1
 
-    left3 = new CANSparkMax(6, MotorType.kBrushless);
-    left3.follow(left1);
-
-    right1 = new CANSparkMax(1, MotorType.kBrushless);
+    right1 = new WPI_TalonSRX(4);
     right1.setInverted(true);
     // rightController = right1.getPIDController();
     // setControler(rightController);
 
-    right2 = new CANSparkMax(2, MotorType.kBrushless);
+    right2 = new WPI_TalonSRX(5);
     right2.follow(right1);
-
-    right3 = new CANSparkMax(3, MotorType.kBrushless);
-    right3.follow(right1);
   }
 
   public void setPower(Double leftPower, Double rightPower) {
     left1.set(leftPower);
     left2.set(leftPower);
-    left3.set(leftPower);
     right1.set(rightPower);
     right2.set(rightPower);
-    right3.set(rightPower);
   }
 
   @Override
