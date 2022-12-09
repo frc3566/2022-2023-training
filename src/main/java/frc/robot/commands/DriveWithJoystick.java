f13ab6952c3713ec224bad431d46ddf3a8a984f8
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-12-09 16:14:26 -0500

    update

--- src/main/java/frc/robot/commands/DriveWithJoystick.java
+++ src/main/java/frc/robot/commands/DriveWithJoystick.java
@@ -1,9 +1,9 @@
 package frc.robot.commands;
 
+import frc.robot.subsystems.DriveSubsystem;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
-import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveWithJoystick extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
